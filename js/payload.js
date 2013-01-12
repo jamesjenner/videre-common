fafa03efd61fa6af479178898c4e594822b822cb
James Jenner <james.g.jenner@gmail.com>
2013-01-12 15:23:16 +1000

    Removed use strict until issues with node.js are sorted out

--- js/payload.js
+++ js/payload.js
@@ -16,7 +16,7 @@
  * along with this program. If not, see http://www.gnu.org/licenses/
  */
 
-"use strict"
+// "use strict"
 
 var Payload = function (options) {
     options = options || {};
--- js/vehicle.js
+++ js/vehicle.js
@@ -16,7 +16,7 @@
  * along with this program. If not, see http://www.gnu.org/licenses/
  */
 
-"use strict"
+// "use strict"
 
 var VEHICLE_AIR = 'air';
 var VEHICLE_SURFACE = 'surface';
