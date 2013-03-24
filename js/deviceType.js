9c50ac1b094a42a12bbf7fcf892db42ff490d588
James Jenner <james.g.jenner@gmail.com>
2013-03-24 13:59:09 +1000

    Initial version

---
+++ js/deviceType.js
@@ -0,0 +1,32 @@
+/*
+ * deviceType.js
+ *
+ * Copyright (c) 2013 James G Jenner
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://www.gnu.org/licenses/
+ */
+
+// "use strict"
+
+if(typeof module == "undefined"){
+    var module = function(){};
+    var exports = this['deviceType'] = {};
+    module.exports = exports;
+}
+
+module.exports = DeviceType;
+
+function DeviceType(id, name) {
+    this.id = id;
+    this.name = name;
+}
