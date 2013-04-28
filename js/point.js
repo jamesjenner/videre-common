f9c539ea073a694ddcb08a45c1a62aeedeebe6c4
James Jenner <james.g.jenner@gmail.com>
2013-04-28 17:04:32 +1000

    Added stop, start, autoContinue, accuracy, current, yaw, pitch, loiterLaps

--- js/point.js
+++ js/point.js
@@ -33,12 +33,20 @@ function Point(lat, lng, options) {
     options = options || {};
 
     this.position = new Position(lat, lng);
+    this.stop = options.stop === undefined ? false : options.stop;
+    this.start = options.start === undefined ? false : options.start;
     this.sequence = options.sequence === undefined ? 0 : options.sequence;
     this.altitude = options.altitude === undefined ? 0 : options.altitude;
     this.isHome = options.isHome === undefined ? false : options.isHome;
     this.loiter = options.loiter === undefined ? false : options.loiter;
+    this.autoContinue = options.autoContinue === undefined ? true : options.autoContinue;
+    this.accuracy = options.accuracy === undefined ? 0 : options.accuracy;
+    this.current = options.current === undefined ? false : options.current;
+    this.yaw = options.yaw === undefined ? 0 : options.yaw;
+    this.pitch = options.pitch === undefined ? 0 : options.pitch;
     this.loiterTime = options.loiterTime === undefined ? 0 : options.loiterTime;
     this.loiterRadius = options.loiterRadius === undefined ? 0 : options.loiterRadius;
+    this.loiterLaps = options.loiterLaps === undefined ? 0 : options.loiterLaps;
     this.returnHome = options.returnHome === undefined ? false : options.returnHome;
     this.terminus  = options.terminus  === undefined ? false : options.terminus ;
 }
